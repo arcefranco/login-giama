854be0afe63f84b045b98582d17fca3eb1624385
franco <2>
2022-08-22 17:02:17 -0300

    template email

--- src/helpers/email/transporter.js
+++ src/helpers/email/transporter.js
@@ -1,4 +1,9 @@
 const nodemailer = require('nodemailer')
+const path = require('path')
+const hbs = require('nodemailer-express-handlebars');
+
+
+
 export const transporter = nodemailer.createTransport({ //Credenciales para enviar mail 
     host: "mail.giama.com.ar", //faltan .ENV
     port: 25, 
@@ -13,3 +18,15 @@ export const transporter = nodemailer.createTransport({ //Credenciales para envi
       rejectUnauthorized: false 
   }
   });
+
+  const handlebarOptions = {
+    viewEngine: {
+      extName: ".handlebars",
+      partialsDir:  path.resolve('./views'),
+      defaultLayout: false
+    },
+    viewPath: path.resolve('./views'),
+    extName: ".handlebars"
+  }
+
+  transporter.use('compile', hbs(handlebarOptions))
