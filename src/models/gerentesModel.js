26a6d0046a5abace2f24e6898b95291b77812a45
franco <2>
2022-08-29 17:49:26 -0300

    fix bug

--- src/models/gerentesModel.js
+++ src/models/gerentesModel.js
@@ -1,14 +1,10 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import {Sequelize,DataTypes} from 'sequelize'
+
+/* import {Sequelize,DataTypes} from 'sequelize'
 require('dotenv').config()
-const sequelize = new Sequelize(process.env.DB_NAME_CG, process.env.DB_USERNAME, process.env.DB_PASSWORD,{
-=======
-=======
->>>>>>> a6e892781473e6a6766027aff8505cc7e4f805ab
 import {Sequelize, DataTypes} from 'sequelize'
 import {app} from '../index'
 require('dotenv').config()
+const sequelize = new Sequelize(process.env.DB_NAME_CG, process.env.DB_USERNAME, process.env.DB_PASSWORD */
 /*  const sequelize = new Sequelize(.Sequelize.config.database, process.env.DB_USERNAME, process.env.DB_PASSWORD,{
 <<<<<<< HEAD
 >>>>>>> 4faf8d98168d5354dcfb5ba1424d667da5d76ce9
