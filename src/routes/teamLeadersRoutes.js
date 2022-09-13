f4020b293072992e0932149a33af70b96ff40d9b
cmelgargiama <cmelgar@giama.com.ar>
2022-09-13 11:04:50 -0300

    getAllActivos update

--- src/routes/teamLeadersRoutes.js
+++ src/routes/teamLeadersRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {  getTeamLeaders, getTeamLeadersById, postTeamLeaders, updateTeamLeaders,deleteTeamLeaders, getAllZonas, endCommit } from "../controllers/teamLeadersController";
+import {  getTeamLeaders, getTeamLeadersById, postTeamLeaders, updateTeamLeaders,deleteTeamLeaders, getAllZonas, endCommit, getTeamLeadersActivos } from "../controllers/teamLeadersController";
 import {  getGerentes} from "../controllers/gerentesController";
 import { errorHandling } from "../middlewares/errorHandling";
 
@@ -9,11 +9,11 @@ const TeamLeadersRouter = Router()
 TeamLeadersRouter.use(errorHandling)
 
 TeamLeadersRouter.route('/').get(getTeamLeaders);
+TeamLeadersRouter.route('/activos').get(getTeamLeadersActivos);
 TeamLeadersRouter.route('/id').post(getTeamLeadersById);
 TeamLeadersRouter.route('/').post(postTeamLeaders);
 TeamLeadersRouter.route('/').put(updateTeamLeaders);
 TeamLeadersRouter.route('/').delete(deleteTeamLeaders);
 TeamLeadersRouter.get('/endCommit', endCommit);
-TeamLeadersRouter.route('/gerentes').get(getGerentes);
 
 export default TeamLeadersRouter
