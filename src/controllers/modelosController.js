0c69e4fa7928db0fee0928aa56b47fd8ae3e93df
cmelgargiama <cmelgar@giama.com.ar>
2022-09-19 17:11:00 -0300

    modelosController

--- src/controllers/modelosController.js
+++ src/controllers/modelosController.js
@@ -6,12 +6,6 @@ import awaitWithTimeout from '../helpers/transaction/awaitWithTimeout'
 
 let transaction;
 
-// export const getModelos = async (req, res) => {
-//     const dbGiama = app.get('db')
-    
-//     const allModelos = await dbGiama.query("SELECT Codigo, Nombre, Activo, NacionalImportado  FROM modelos  " )
-//     res.send(allModelos)
-// }
 
 export const getTipoPlan = async (req, res) => {
     const dbGiama = app.get('db')
@@ -22,12 +16,6 @@ export const getTipoPlan = async (req, res) => {
 
 export const getModelos = async (req, res) => {
     const dbGiama = app.get('db')
-    let i = 1;
-    let object;
-    let modelos = '';
-    const allModels = await dbGiama.query(`SELECT * FROM modelosvalorescuotas`);
-    // console.log(allModels[0].length)
-    // while( i< 10 ){
     const allModelos = await dbGiama.query(`
     SELECT modelos.Marca, modelos.Codigo, modelos.Nombre, modelos.Coeficiente, 
                 modelos.FechaAltaRegistro, modelos.UsuarioAltaRegistro, modelos.NacionalImportado, modelos.Activo,
@@ -38,104 +26,12 @@ export const getModelos = async (req, res) => {
                 LEFT JOIN modelosvalorescuotas ON modelos.Marca = modelosvalorescuotas.Marca 
                 AND modelos.Codigo = modelosvalorescuotas.Codigo 
                 LEFT JOIN tipoplan ON modelosvalorescuotas.tipoplan = tipoplan.ID
-
-
-    
-         
          `)
        
-    // res.send(allModelos)
-    // const allModelosPlan1 = await dbGiama.query(`
-    // SELECT modelosvalorescuotas.Codigo AS Codigo,
-    // tipoplan.Descripcion AS 'tipoPlan',
-    // modelosvalorescuotas.CuotaTerminal,
-    //  modelosvalorescuotas.CuotaACobrar,
-    //  modelosvalorescuotas.CuotaACobrarA,
-    //  modelosvalorescuotas.Cuota1,
-    //   modelosvalorescuotas.Cuota2 
-    //   FROM modelosvalorescuotas
-    //   LEFT JOIN tipoplan 
-    //   ON modelosvalorescuotas.TipoPlan = tipoplan.ID
-    //   WHERE Codigo = ? AND tipoPlan = 1`,
-    //   {
-    //       replacements:[10],
-    //       type:QueryTypes.SELECT,
-
-    //   })
-    // // res.send(allModelosPlan1)
-    // const allModelosPlan2 = await dbGiama.query(`SELECT 
-    // modelosvalorescuotas.Codigo AS Codigo,
-    // modelosvalorescuotas.CuotaTerminal,
-    // modelosvalorescuotas.CuotaACobrar,
-    // modelosvalorescuotas.CuotaACobrarA,
-    // modelosvalorescuotas.Cuota1,
-    // modelosvalorescuotas.Cuota2
-    // FROM modelosvalorescuotas
-    // WHERE Codigo = ? AND tipoPlan = 2`
-    // ,
-    //     {
-    //         replacements:[10],
-    //         type:QueryTypes.SELECT,
-
-    //     })
-    
-    // // res.send(allModelosPlan2)
-    // const modelo = allModelos[0]
-    // const plan1 = allModelosPlan1[0]
-    // const plan2 = allModelosPlan2[0]
-//         if(!allModelosPlan1[0] || !allModelosPlan2[0] ){
-//     object = {
-//         CuotaTerminalP1:'',
-//         CuotaACobrarP1: '',
-//         CuotaACobrarAP1:'',
-//         Cuota1P1:'',
-//         Cuota2P1:'',
-//         CuotaTerminalP2:'',
-//         CuotaACobrarP2:'',
-//         CuotaACobrarAP2:'',
-//         Cuota1P2:'',
-//         Cuota2P2:'',
-// }} 
-    // object = {Codigo:modelo.Codigo,
-    //         Activo:modelo.Activo,
-    //         Nombre:modelo.Nombre,
-    //         NacionalImportado:modelo.NacionalImportado,
-    //         CuotaTerminalP1:plan1.CuotaTerminal,
-    //         CuotaACobrarP1:plan1.CuotaACobrar,
-    //         CuotaACobrarAP1:plan1.CuotaACobrarA,
-    //         Cuota1P1:plan1.Cuota1,
-    //         Cuota2P1:plan1.Cuota2,
-    //         CuotaTerminalP2:plan2.CuotaTerminal,
-    //         CuotaACobrarP2:plan2.CuotaACobrar,
-    //         CuotaACobrarAP2:plan2.CuotaACobrarA,
-    //         Cuota1P2:plan2.Cuota1,
-    //         Cuota2P2:plan2.Cuota2,
-    // }
-// }
-// modelos = modelos + JSON.stringify(object);
-    // i++
-    //  }    
-    
     
     res.send(allModelos)    
-    // const modelosCuotas = await dbGiama.query("SELECT modelosvalorescuotas.`CuotaTerminal`, modelosvalorescuotas.`CuotaACobrar`, modelosvalorescuotas.`CuotaACobrarA`, modelosvalorescuotas.`Cuota1`, modelosvalorescuotas.`Cuota2`FROM modelosvalorescuotas WHERE Codigo = ? ",{
-    //     replacements:[allModelos],
-    //     type:QueryTypes.SELECT
-    // })
-    // res.send({
-    //     Codigo:allModelos.Codigo,
-    //     Nombre:allModelos.Nombre,
-    //     Cuotas: modelosCuotas,
-    // })
 }
 
-// export const getModelosActivos = async (req, res) => {
-//     const dbGiama = app.get('db')
-    
-//     const allModelos = await dbGiama.query("SELECT Codigo, Nombre, Activo FROM modelos WHERE Activo = 1")
-//     res.send(allModelos)
-
-// }
 
 export const getModelosById = async (req, res) => {
     const Modelos = req.body
@@ -148,17 +44,31 @@ export const getModelosById = async (req, res) => {
       })
       const query = () => {
         return new Promise((resolve, reject) => {
-    const allModelosById =  ModelosModel.findAll(
+    const allModelosById =  dbGiama.query(
+        `SELECT modelos.Marca, modelos.Codigo, modelos.Nombre, modelos.Coeficiente, 
+        modelos.FechaAltaRegistro, modelos.UsuarioAltaRegistro, modelos.NacionalImportado, modelos.Activo,
+        modelosvalorescuotas.tipoplan AS Codtipoplan, tipoplan.Descripcion AS Nomtipoplan, 
+        modelosvalorescuotas.CuotaTerminal, modelosvalorescuotas.CuotaACobrar, modelosvalorescuotas.CuotaACobrarA, 
+        modelosvalorescuotas.Cuota2, modelosvalorescuotas.Cuota1 
+        FROM pa7.modelos 
+        LEFT JOIN modelosvalorescuotas ON modelos.Marca = modelosvalorescuotas.Marca 
+        AND modelos.Codigo = modelosvalorescuotas.Codigo 
+        LEFT JOIN tipoplan ON modelosvalorescuotas.tipoplan = tipoplan.ID
+        WHERE modelosvalorescuotas.Codigo = ?`,
         {
         transaction: transaction,
-        where:{Codigo:Modelos.Codigo}
+        replacements:[Modelos.Codigo],
+        type: QueryTypes.SELECT
     })
     console.log(allModelosById)
     resolve(allModelosById)
 
     
-})
+})    
 }
+
+
+
 const response = await awaitWithTimeout(4000, query()) 
 
 res.send(response)
@@ -201,8 +111,8 @@ try{    await dbGiama.query("INSERT INTO modelos (Nombre, Activo, UsuarioAltaReg
     
  
 export const updateModelos = async (req, res) => {
-    const Modelos = req.body;
-    console.log(Modelos)
+    const {Codigo, Nombre, Activo, NacionalImportado, CuotaTerminal, CuotaACobrarA, CuotaACobrar, Cuota1, Cuota2} = req.body;
+    console.log(req.body)
     const dbGiama = app.get('db')
     const user = req.body.HechoPor;
     try {
@@ -223,12 +133,18 @@ export const updateModelos = async (req, res) => {
     const Modelo = app.get('db').models.modelos;
     try{ await Modelo?.update(
     {
-        Nombre: Modelos.Nombre,
-        Activo: Modelos.Activo,
+        Nombre: Nombre,
+        Activo: Activo,
+        NacionalImportado: NacionalImportado,
+        CuotaTerminal: CuotaTerminal,
+        CuotaACobrar: CuotaACobrar,
+        CuotaACobrarA: CuotaACobrarA,
+        Cuota1: Cuota1,
+        Cuota2: Cuota2,
         UsuarioAltaRegistro: user,
     }
     ,{
-        where: {Codigo: Modelos.Codigo}
+        where: {Codigo: Codigo}
     });
     return res.send({status: true, data: 'Modelo actualizado correctamente!'})
         
