c83029f3368168366a5dc835f34d9ce7f627e526
franco <2>
2023-02-01 10:40:34 -0300

    fixing controllers & usuarios controller

--- src/controllers/queries/beginUpdateQuery.js
+++ src/controllers/queries/beginUpdateQuery.js
@@ -27,3 +27,30 @@ export const beginUpdateQuery = async (db, usuario, codigo, table) => {
         throw {status: false, message: returnErrorMessage(error)}
     }
 }
+
+export const beginUpdateQueryID = async (db, usuario, codigo, table) => {
+
+    const dbGiama = db
+    const user = usuario
+    if(!codigo){
+        throw {status: false, message: `ID required`}
+        }
+    try {
+        const actualUsuario = await dbGiama.query(`SELECT inUpdate FROM ${table} WHERE ID = ?`, 
+        {
+            replacements: [codigo],
+            type: QueryTypes.SELECT
+        })
+        if(actualUsuario[0].inUpdate === null  || actualUsuario[0].inUpdate === user){
+            await dbGiama.query(`UPDATE ${table} SET inUpdate = ? WHERE ID = ?`, {
+                replacements: [user, codigo],
+                type: QueryTypes.UPDATE
+            })
+            return {codigo: codigo}
+        }else{
+            throw {status: false, message: `El registro está siendo editado por ${actualUsuario[0].inUpdate}`}
+        }
+    } catch (error) {
+        throw {status: false, message: returnErrorMessage(error)}
+    }
+}
