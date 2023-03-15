2d0f831823b665762262d991e5f75edb2c576fab
franco <2>
2023-03-15 17:42:03 -0300

    efectividadAdj detalles

--- src/routes/Operaciones/efectividadAdjRoutes.js
+++ src/routes/Operaciones/efectividadAdjRoutes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import {
   getEfectividadAdj,
   getOficialAdj,
+  getDetalleEfectividad,
 } from "../../controllers/Operaciones/efectividadAdjController";
 import { testConnection } from "../../middlewares/testConnection";
 import { testConnectionAll } from "../../middlewares/testConnectionAll";
@@ -13,4 +14,5 @@ const efectividadAdj = Router();
 efectividadAdj.use(testConnection);
 efectividadAdj.post("/", testConnectionAll, getEfectividadAdj);
 efectividadAdj.route("/oficiales").get(getOficialAdj);
+efectividadAdj.route("/detalle").post(getDetalleEfectividad);
 export default efectividadAdj;
